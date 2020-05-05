25b35a4bf9490ec58a980ff6554d2b7cb95209ed
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-05-05 10:20:24 +0200

    added comments to weather_repository package

--- weather_repository_core/lib/src/weather_repository.dart
+++ weather_repository_core/lib/src/weather_repository.dart
@@ -4,9 +4,20 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app_example_data_models_core/weather_app_example_data_models_core.dart';
 
+/// This repository provides methods to access data from [MetaWeather](https://www.metaweather.com/).
+///
+/// If you want to request weather data
+/// use the method [getWeatherFor(String city)].
+///
+/// Example:
+/// ```dart
+///   await weatherRepository.getWeatherFor(city: 'London');
+/// ```
 class WeatherRepository {
+  /// [baseUrl] holds a reference to MetaWeather's API.
   static const baseUrl = 'https://www.metaweather.com';
 
+  /// Returns a `Location` for the requested [city].
   Future<Location> fetchLocationFor({@required String city}) async {
     final locationUrl = '$baseUrl/api/location/search/?query=$city';
     final locationResponse = await http.get(locationUrl);
@@ -19,6 +30,9 @@ class WeatherRepository {
     return Location.fromJson(locationJson.first);
   }
 
+  /// Returns a `WeatherResponse` for a [Location].
+  ///
+  /// Throws an [Exception] when the server response code is **not** 200.
   Future<WeatherResponse> fetchWeatherResponseForLocation(
       {@required Location location}) async {
     final weatherUrl = '$baseUrl/api/location/${location.woeId}';
@@ -30,9 +44,12 @@ class WeatherRepository {
     return WeatherResponse.fromJson(weatherJson);
   }
 
+  /// Returns a `WeatherResponse` for a `Location`.
+  ///
+  /// Use this function when you want to request weather data.
   Future<WeatherResponse> getWeatherFor({@required String city}) async {
-    final Location _location = await fetchLocationFor(city: city);
-    final WeatherResponse _weatherResponse =
+    final _location = await fetchLocationFor(city: city);
+    final _weatherResponse =
         await fetchWeatherResponseForLocation(location: _location);
     return _weatherResponse;
   }
--- weather_repository_core/lib/weather_repository_core.dart
+++ weather_repository_core/lib/weather_repository_core.dart
@@ -1,3 +1,14 @@
+/// This library provides weather data from [MetaWeather](https://www.metaweather.com/).
+///
+/// To receive the weather data for a city use the `getWeatherFor` method.
+///
+/// The following code snippet shows how to retrieve the weather data
+/// for London:
+/// ```dart
+/// ...
+///   await getWeatherFor(city: 'London');
+/// ...
+/// ```
 library weather_repository_core;
 
 export 'src/weather_repository.dart';
