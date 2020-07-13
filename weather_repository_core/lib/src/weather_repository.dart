6d259a9c4f1956cc37734396cbb78401854fb4a2
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-07-13 14:22:26 +0200

    added method to fetch weather data for lattitude and longitude

--- weather_repository_core/lib/src/weather_repository.dart
+++ weather_repository_core/lib/src/weather_repository.dart
@@ -18,12 +18,26 @@ class WeatherRepository {
   static const baseUrl = 'https://www.metaweather.com';
 
   /// Returns a `Location` for the requested [city].
-  Future<Location> fetchLocationFor({@required String city}) async {
+  Future<Location> fetchLocationWithQuery({@required String city}) async {
     final locationUrl = '$baseUrl/api/location/search/?query=$city';
     final locationResponse = await http.get(locationUrl);
 
     if (locationResponse.statusCode != 200) {
       throw Exception('error getting locationId for $city');
+    }
+
+    final locationJson = jsonDecode(locationResponse.body) as List;
+    return Location.fromJson(locationJson.first);
+  }
+
+  /// Returns a `Location` for the given [lattitude] and [longitude].
+  Future<Location> fetchLocationWithLattLong(
+      {@required double latt, @required double long}) async {
+    final locationUrl = '$baseUrl/api/location/search/?lattlong=$latt,$long';
+    final locationResponse = await http.get(locationUrl);
+
+    if (locationResponse.statusCode != 200) {
+      throw Exception('error getting locationId for $latt,$long');
     }
 
     final locationJson = jsonDecode(locationResponse.body) as List;
@@ -44,11 +58,22 @@ class WeatherRepository {
     return WeatherResponse.fromJson(weatherJson);
   }
 
-  /// Returns a `WeatherResponse` for a `Location`.
+  /// Returns a `WeatherResponse` for a `Location` with a query.
   ///
-  /// Use this function when you want to request weather data.
-  Future<WeatherResponse> getWeatherFor({@required String city}) async {
-    final _location = await fetchLocationFor(city: city);
+  /// Use this function when you want to request weather data with a query.
+  Future<WeatherResponse> getWeatherWithQuery({@required String city}) async {
+    final _location = await fetchLocationWithQuery(city: city);
+    final _weatherResponse =
+        await fetchWeatherResponseForLocation(location: _location);
+    return _weatherResponse;
+  }
+
+  /// Returns a `WeatherResponse` for a `Location` with a query.
+  ///
+  /// Use this function when you want to request weather data with a query.
+  Future<WeatherResponse> getWeatherWithLattLong(
+      {@required double latt, @required double long}) async {
+    final _location = await fetchLocationWithLattLong(latt: latt, long: long);
     final _weatherResponse =
         await fetchWeatherResponseForLocation(location: _location);
     return _weatherResponse;
